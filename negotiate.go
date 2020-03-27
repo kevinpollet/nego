34b98d9d6cae918c76f79f111ff95a9e79c0e173
kevinpollet <pollet.kevin@gmail.com>
2020-03-27 09:31:39 +0400

    docs: add package doc and simplify RFC links

--- negotiate.go
+++ negotiate.go
@@ -1,3 +1,22 @@
+// Package nego implements HTTP Content Negotiation functions compliant with RFC 7231.
+//
+// See https://tools.ietf.org/html/rfc7231#section-5.3 for more details.
+//
+// Example
+//
+// This example shows how to use the negotiation functions.
+//
+//	import (
+//		"net/http"
+//		"github.com/kevinpollet/nego"
+//	)
+//
+// 	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+// 		nego.NegotiateContentCharset(req, "utf-8")
+// 		nego.NegotiateContentEncoding(req, "gzip", "deflate")
+// 		nego.NegotiateContentLanguage(req, "fr", "en")
+// 		nego.NegotiateContentType(req, "text/plain")
+// 	})
 package nego
 
 import "net/http"
@@ -5,14 +24,14 @@ import "net/http"
 // The identity encoding constant used as a synonym for "no encoding" in order to communicate when
 // no encoding is preferred.
 //
-// See https://tools.ietf.org/html/rfc7231#section-5.3.4
+// See https://tools.ietf.org/html/rfc7231#section-5.3.4 for more details.
 const EncodingIdentity = "identity"
 
 // NegotiateContentCharset returns the best acceptable charset offer to use in the response according
 // to the Accept-Charset request's header. If the given offer list is empty or no offer is acceptable
 // then, an empty string is returned.
 //
-// See section 5.3.3 of RFC 7321 for more details: https://tools.ietf.org/html/rfc7231#section-5.3.3
+// See https://tools.ietf.org/html/rfc7231#section-5.3.3 for more details.
 func NegotiateContentCharset(req *http.Request, offerCharsets ...string) string {
 	bestQvalue := 0.0
 	bestCharset := ""
@@ -36,7 +55,7 @@ func NegotiateContentCharset(req *http.Request, offerCharsets ...string) string 
 // to the Accept-Encoding request's header. If the given offer list is empty or no offer is acceptable
 // then, an empty string is returned.
 //
-// See section 5.3.4 of RFC 7321 for more details: https://tools.ietf.org/html/rfc7231#section-5.3.4
+// See https://tools.ietf.org/html/rfc7231#section-5.3.4 for more details.
 func NegotiateContentEncoding(req *http.Request, offerEncodings ...string) string {
 	bestQvalue := 0.0
 	bestEncoding := ""
@@ -64,7 +83,7 @@ func NegotiateContentEncoding(req *http.Request, offerEncodings ...string) strin
 // to the Accept-Language request's header. If the given offer list is empty or no offer is acceptable
 // then, an empty string is returned.
 //
-// See section 5.3.5 of RFC 7321 for more details: https://tools.ietf.org/html/rfc7231#section-5.3.5
+// See https://tools.ietf.org/html/rfc7231#section-5.3.5 for more details.
 func NegotiateContentLanguage(req *http.Request, offerLanguages ...string) string {
 	bestQvalue := 0.0
 	bestLanguage := ""
@@ -88,7 +107,7 @@ func NegotiateContentLanguage(req *http.Request, offerLanguages ...string) strin
 // to the Accept-Language request's header. If the given offer list is empty or no offer is acceptable
 // then, an empty string is returned.
 //
-// See section 5.3.2 of RFC 7321 for more details: https://tools.ietf.org/html/rfc7231#section-5.3.2
+// See https://tools.ietf.org/html/rfc7231#section-5.3.2 for more details.
 func NegotiateContentType(req *http.Request, offerMediaTypes ...string) string {
 	bestMediaType := ""
 	bestQvalue := 0.0
